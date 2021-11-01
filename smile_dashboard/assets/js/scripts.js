ef23666b8c32f36ac7a04235cf754f012f0d0947
John1316 <johnmaher179@gmail.com>
2021-11-01 16:20:07 +0200

    update commit

---
+++ smile_dashboard/assets/js/scripts.js
@@ -0,0 +1,27 @@
+(function(window, undefined) {
+  'use strict';
+
+  /*
+  NOTE:
+  ------
+  PLACE HERE YOUR OWN JAVASCRIPT CODE IF NEEDED
+  WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR JAVASCRIPT CODE PLEASE CONSIDER WRITING YOUR SCRIPT HERE.  */
+  $(".progress-bar").loading();
+
+
+  $('.zero-configuration').DataTable( {
+    dom: 'Bfrtip',
+    buttons: [
+        'csv'
+    ]
+});
+  $('#list-bachelor-users-table').DataTable( {
+    dom: 'Bfrtip',
+    buttons: [
+        'csv'
+    ]
+    
+});
+
+
+})(window);
